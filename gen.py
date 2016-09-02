4dae27d26b8a605054d336ce5195f9ddd78345b6
Alex Wang <alex3763@gmail.com>
2016-09-02 13:55:02 -0400

    modifications

--- gen.py
+++ gen.py
@@ -1,7 +1,10 @@
 from random import random
+from time import time
 
 NUMTEAMS = 30
 REPEAT_ROOM = False
+
+tic = time()
 
 rooms = []
 ans = []
@@ -45,5 +48,33 @@ for i in range(NUMTEAMS*NUMTEAMS*len(ans)):
 			assign[team1][x] = assign[team2][x]
 			assign[team2][x] = tmp
 
-print assign
+for i in range(NUMTEAMS):
+	for j in range(len(ans)):
+		if j < (len(ans)-1):
+			tmp = assign[i][j+1]
+			text = ""
+			if ans[j] > tmp: #handle differently for greater than or less than answer
+				a = int(ans[j])/int(tmp)
+				b = int(ans[j])/a - tmp
+				text = "Take your answer, divide by " + str(a) + " and throw away the remainder, and subtract " + str(b) + ". This is your next room number."
+			else if ans[j] < tmp:
+				a = int(tmp)/int(ans[j])
+				b = tmp - a*ans[j]
+				text = "Take your answer, multiply by " + str(a) + ", and add " + str(b) + ". This is your next room number."
+			else: #if equal just go there
+				text = "Your answer is your next room number."
+			assign[i][j] = (assign[i][j],text,tmp)
+		else:
+			assign[i][j] = (assign[i][j],"Return to the cafeteria.","Cafeteria")
 
+fout = open("out.txt","w")
+for i in range(NUMTEAMS):
+	fout.write("TEAM " + str(i+1) + "\n")
+	for j in range(len(ans)):
+		fout.write("Question " + str(j) + " - CURRENT ROOM NUMBER: " + str(assign[i][j][0]) + " DIRECTION: " + str(assign[i][j][1]) + " NEXT ROOM NUMBER: " + str(assign[i][j][2]) + "\n")
+fout.close()
+
+toc = time()
+
+print "Done!"
+print "Time Elapsed: " + str(toc-tic)
